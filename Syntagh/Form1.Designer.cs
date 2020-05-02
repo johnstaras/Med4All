f3e1ede461d4c6b549dc904b1e4c65236900338b
ΓΕΩΡΓΙΟΣ ΤΣΩΛΑΣ <ceid6249@upatras.gr>
2020-05-03 00:27:00 +0300

    ανέβασα τις φώτος

--- Syntagh/Form1.Designer.cs
+++ Syntagh/Form1.Designer.cs
@@ -394,7 +394,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Image = global::Syntagh.Properties.Resources.τελικοαμαν;
             this.pictureBox2.Location = new System.Drawing.Point(218, 259);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(1095, 148);

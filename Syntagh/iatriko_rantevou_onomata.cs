778a8f3a67911e32179b7ffceade3db2892e6d3c
ΚΩΝΣΤΑΝΤΙΝΟΣ ΜΙΧΟΣ <ceid6325@upatras.gr>
2020-05-03 15:40:24 +0300

    ιατρικο ραντεβου ονοματα ιατρων

---
+++ Syntagh/iatriko_rantevou_onomata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Syntagh
+{
+    public partial class iatriko_rantevou_onomata : Form
+    {
+        public iatriko_rantevou_onomata()
+        {
+            InitializeComponent();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

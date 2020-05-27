08d4224d6044d6121585ad5556c756535a3cbd23
ΑΡΗΣ ΝΑΚΟΣ <nakosar96@yahoo.com>
2020-05-27 13:56:55 +0300

    class {xarths +efimereuwn}

---
+++ Syntagh/Xarth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    class Xarth
+    {
+        private string topothesia_efimereuwn;
+        private string topothesia_farmakeiwn;
+        private string onoma_farmakeiwn;
+        private string onoma_efimereuwn_farmakeiwn;
+        private int arithmos_krousmatwn;
+        private string kathgoria_pathogonou;
+
+        public string get_topothesia() 
+        { 
+            private string[] topothesies_onomata_efimereuwn;
+            topothesies_onomata_efimereuwn[0] = this.topothesia_efimereuwn;
+            topothesies_onomata_efimereuwn[1] = this.onoma_efimereuwn_farmakeiwn;
+
+            return topothesies_onomata_efimereuwn[];
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Syntagh/efhmereuwn_farmakeia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    class efhmereuwn_farmakeia
+    {
+        private string topothesies;
+        private string onomasies;
+
+        public void call() 
+        
+        { 
+        
+        
+        }
+
+        public string check_address() 
+        {
+        private string topothesies = this.topothesies;
+        
+        return topothesies;
+        
+        }
+
+
+
+
+    }
+}

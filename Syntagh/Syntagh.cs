9f55340e80513d400b64e86935f675358b5775f3
kosgots <48559134+kosgots@users.noreply.github.com>
2020-05-27 17:34:11 +0300

    gotsoulias syntagh_paraggelia_plhrofories classes

---
+++ Syntagh/Syntagh.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    class Syntagh
+    {
+        private string syntages;
+        private string eksetaseis;
+        private string created_from;
+        private string created_for;
+        private string expires;
+        private int id;
+
+
+
+
+        public void createForm(string syntages, string created_from, string expires, string created_for, int id)
+        {
+            this.syntages = syntages;
+            this.eksetaseis = eksetaseis;
+            this.created_from = created_from;
+            this.created_for = created_for;
+            this.expires = expires;
+            this.id = id;
+        }
+
+
+        static int getSyntagh(int id)
+        {
+            return id;
+        }
+
+        static void UpdateForm(string syntages, string created_from, string expires, string created_for)
+        {
+
+        }
+
+        static bool oloklhrwmenes(string syntages)
+        {
+            bool finished = true;
+            return finished;
+
+        }
+
+    }
+}
---
+++ Syntagh/paraggelia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    class paraggelia
+    {
+        private string onoma_pelath;
+        private string onoma_farmakopoiou;
+        private float xronos_etoimothtas;
+        private int id;
+
+
+        public void addPaggelia(int id, string onoma_pelath, string onoma_farmakopoiou, float xronos_etoimothtas)
+        {
+            this.onoma_pelath = onoma_pelath;
+            this.onoma_farmakopoiou = onoma_farmakopoiou;
+            this.xronos_etoimothtas = xronos_etoimothtas;
+            this.id = id;
+        }
+
+    }
+}

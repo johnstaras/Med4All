f5e6a1c6e9bc221e6c698ee9078dfbcb11e479f7
ΓΕΩΡΓΙΟΣ ΤΣΩΛΑΣ <ceid6249@upatras.gr>
2020-05-29 02:03:56 +0300

    οι κλασεις. θέλουν μερικα ακόμα

---
+++ Syntagh/aksiologhseis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    public class aksiologhseis
+    {
+        float asteria;
+        string keimeno;
+
+        void create_aksiologhsh()
+        {
+            string sxolio;
+            float stars;
+            sxolio = Console.ReadLine();
+            stars = float.Parse(Console.ReadLine());
+            if (stars < 0)
+            {
+                stars = 0;
+            }
+            else if (stars > 5)
+            {
+                stars = 5;
+            }
+
+            this.asteria = stars;
+            this.keimeno = sxolio;
+
+        }
+    }
+}
---
+++ Syntagh/diagnwshclass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntagh
+{
+    public class diagnwshclass
+    {
+        float pyretos, nero;
+        int hlikia;
+        Boolean ponokefalos;
+        string symperasma;
+
+        public string evaluate_symptoms()
+        {
+            string diagnwsh = "";
+            if (hlikia < 50)
+            {
+                if (this.ponokefalos == true & this.pyretos > 36.6)
+                {
+                    if (nero > 1)
+                    {
+                        diagnwsh = "Κρύωμα";
+                    }
+                    else
+                    {
+                        diagnwsh = "Αφυδάτωση";
+                    }
+                }
+                else if (this.ponokefalos == true & this.pyretos == 36.6)
+                {
+                    diagnwsh = "Κόπωση΄";
+                }
+            }else
+            {
+                if (this.ponokefalos == true & this.pyretos > 36.6)
+                {
+                    if (nero > 1)
+                    {
+                        diagnwsh = "Ίωση";
+                    }
+                    else
+                    {
+                        diagnwsh = "Ηλίαση";
+                    }
+                }
+                else if (this.ponokefalos == true & this.pyretos == 36.6)
+                {
+                        diagnwsh = "Ίλιγγος";
+                }
+
+            }
+            return diagnwsh;
+        }
+    }
+}

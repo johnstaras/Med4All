8f2e352069063eaea7498976961c1185e5b70d39
ΚΩΝΣΤΑΝΤΙΝΟΣ ΜΙΧΟΣ <ceid6325@upatras.gr>
2020-05-28 23:51:49 +0300

    update michos

--- Syntagh/logariasmoi.cs
+++ Syntagh/logariasmoi.cs
@@ -21,6 +21,19 @@ namespace Syntagh
             this.type_of_relation = type_of_relation;
             this.first_name = first_name;
             this.surname = surname;
+
+            string[] account = new string[50000000];
+
+            for (int i = 0; i < account.Length; i = i + 5)
+            {
+
+                account[i - 5] = id;
+                account[i - 4] = password;
+                account[i - 3] = email;
+                account[i - 2] = type_of_relation;
+                account[i - 1] = first_name;
+                account[i] = surname;
+            }
         }
 
 
@@ -28,8 +41,9 @@ namespace Syntagh
         public void delete_account(int id)
         {
 
-            delete_all(id);
-
+            int iSearch = id;
+            string foundIndex = account.ToList().IndexOf(iSearch);
+            account = account.Where(val => val != foundIndex).ToArray();
         }
 
 
--- Syntagh/user.cs
+++ Syntagh/user.cs
@@ -16,35 +16,108 @@ namespace Syntagh
 
         private string syntages;
 
+        string[] mydoctors = new string[50000000];
+
+        string[] mysyntages = new string[50000000];
 
 
-        public string getProfile() { 
+        public string getProfile(id) {
+
+
+            for (int i = 0; i < account.Length; i = i + 5)
+            {
+                if (account[i - 5] = id)
+                {
+                    id = account[i - 5];
+                    password = account[i - 4];
+                    email = account[i - 3];
+                    type_of_relation = account[i - 2];
+                    first_name = account[i - 1];
+                    surname = account[i];
+
+                    onoma_pelath = surname;
+                    topothesia = type_of_relation;
+
+                    return onoma_pelath,topothesia;
+
+                }
+                else {
+                    Console.WriteLine("account no found");
+                }
+            }
+        }
+
+
+        public string add_doctors(id,onoma_giatrou)
+        {
+            for (int i = 0; i < account.Length; i = i + 5)
+            {
+                if (account[i - 5] = id)
+                {
+
+                    for (int i = 0; i < mydoctors.Length; i = i + 2)
+                    {
+                        mydoctors[i-1] = id;
+                        mydoctors[i] = onoma_giatrou;
+
+                    }
+                }
+            }
+        }
         
-        
-        }
-
-
-        public string add_doctors()
+        public string my_doctors(id)
         {
 
+            for (int i = 0; i < mydoctors.Length; i = i + 2)
+            {
+                if (mydoctors[i - 1] = id)
+                {
+
+                    onoma_giatrou = mydoctors[i];
+                    return onoma_giatrou;
+                }
+                else {
+
+
+                    Console.WriteLine("no found");
+
+                }
+
+            }
+
+
 
         }
 
-        public string my_doctors()
+        public string add_syntagh(id, syntages)
         {
 
-
+            for (int i = 0; i < mysyntages.Length; i = i + 2)
+            {
+                mysyntages[i - 1] = id;
+                mysyntages[i] = syntages;
+            }
         }
-
-        public string add_syntagh()
+        public string get_my_syntages(id)
         {
 
+            for (int i = 0; i < mysyntages.Length; i = i + 2)
+            {
+                if (mysyntages[i - 1] = id)
+                {
 
-        }
+                    syntages = mysyntages[i];
+                    return syntages;
+                }
+                else {
 
-        public string get_my_syntages()
-        {
+                    Console.WriteLine("no found syntages");
 
+
+                }
+
+
+            }
 
         }
 

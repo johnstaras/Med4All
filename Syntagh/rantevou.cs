ecc2177e19273e9317d23ed2167a5acc96215127
ΓΕΩΡΓΙΟΣ ΤΣΩΛΑΣ <ceid6249@upatras.gr>
2020-05-29 02:33:56 +0300

    τελικες κλασεις

--- Syntagh/rantevou.cs
+++ Syntagh/rantevou.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,20 @@ namespace Syntagh
         int id;
 
         static string get_wres()
-        { }
-        static void create_appointment()
-        { }
-        static void call_appointment()
-        { }
+        {
+            string wres_giatrou = Giatros.check_hours();
+            return wres_giatrou;
+        }
+        void create_appointment()
+        {
+            onoma_giatrou = this.onoma_giatrou;
+            onoma_pelath = this.onoma_pelath;
+            hmeromhnia_kai_wra = this.hmeromhnia_kai_wra;
+        }
+        void call_appointment()
+        {
+            string call_giatros = this.onoma_giatrou;
+            string call_pelaths = this.onoma_pelath;
+        }
     }
 }
